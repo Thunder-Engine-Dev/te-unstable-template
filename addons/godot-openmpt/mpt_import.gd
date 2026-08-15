7529a212a0db8dc4020c36937a2d4bf07245bbb0
Usered238 <30762980+Usered238@users.noreply.github.com>
2026-08-15 21:06:01 +0900

    fix: icon for audio module files in file system

--- addons/godot-openmpt/mpt_import.gd
+++ addons/godot-openmpt/mpt_import.gd
@@ -2,10 +2,20 @@
 extends EditorPlugin
 
 var import_plugin: EditorImportPlugin = null
+const ICON_PATH = "res://addons/godot-openmpt/AudioStreamMPT.png"
 
 func _enter_tree() -> void:
 	import_plugin = preload("mpt_importer.gd").new()
 	add_import_plugin(import_plugin)
+
+func _ready() -> void:
+	var base_control = EditorInterface.get_base_control()
+	if !base_control.theme:
+		base_control.theme = Theme.new()
+	var custom_icon_texture = load(ICON_PATH)
+	
+	if custom_icon_texture:
+		base_control.theme.set_icon("AudioStreamMPT", "EditorIcons", custom_icon_texture)
 
 
 func _exit_tree() -> void:
